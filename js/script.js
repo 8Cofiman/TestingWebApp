57c7cca4cd91df6b4460b8e0f3a45e69ac5a17df
Pavel Khristenko <102798090+8Cofiman@users.noreply.github.com>
2023-04-24 04:21:14 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnPrev = document.querySelector('.btn-prev');
+  const btnNext = document.querySelector('.btn-next');
+  const slider = ItcSlider.getOrCreateInstance('.slider', { loop: true });
+
+  btnPrev.onclick = () => {
+    slider.prev();
+  }
+  btnNext.onclick = () => {
+    slider.next();
+  }
+});
